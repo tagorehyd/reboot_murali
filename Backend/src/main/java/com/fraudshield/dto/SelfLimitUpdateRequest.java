54e3099e3a2d1f8ef8d49b22777711759aa6f74d
FraudShield Developer <developer@fraudshield.local>
2026-07-29 11:32:42 +0530

    fix(limits): add JsonAlias to SelfLimitUpdateRequest to validate dailyLimit and weeklyLimit field aliases

--- Backend/src/main/java/com/fraudshield/dto/SelfLimitUpdateRequest.java
+++ Backend/src/main/java/com/fraudshield/dto/SelfLimitUpdateRequest.java
@@ -1,12 +1,20 @@
 package com.fraudshield.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 @Data
 public class SelfLimitUpdateRequest {
+    @JsonAlias({"dailyLimit", "dailyTransactionLimit"})
     private Double dailyTransactionLimit;
+
+    @JsonAlias({"weeklyLimit", "weeklyTransactionLimit"})
     private Double weeklyTransactionLimit;
+
+    @JsonAlias({"maxBeneficiary", "maxBeneficiaryAmount"})
     private Double maxBeneficiaryAmount;
+
     private Boolean domesticTransactionsEnabled;
     private Boolean internationalTransactionsEnabled;
 }
+
